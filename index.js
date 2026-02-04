5a549be7a4452ff78c471e71423c376045c0b31b
Dharamveer Singh <dveersingh000@gmail.com>
2026-02-04 13:57:37 +0530

    updated index file for deployment

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.use('/api', require('./src/routes/recommendation.routes'));
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running locally on port ${PORT}`);
+  });
+}
+
+module.exports = app;
